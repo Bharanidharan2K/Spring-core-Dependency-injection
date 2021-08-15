48b2bc58cec54901fd376b4aef7e7815153134f3
Bharanidharan2K <bharanikkab123@gmail.com>
2021-08-15 19:56:29 +0530

    Dependency Injection using setter

--- SpringCore/src/main/java/com/spring/di/Student.java
+++ SpringCore/src/main/java/com/spring/di/Student.java
@@ -6,11 +6,9 @@ public class Student {
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
-
 	public void displayStudentInfo() {
 		System.out.println("Student ID is : "+id);
 		System.out.println("Student Name is : "+ studentName);
